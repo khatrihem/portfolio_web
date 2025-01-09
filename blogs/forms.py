3f8bf115b9374886f09597f48a9dc5a8c0a315ba
khatrihem <hemrkhatri@gmail.com>
2025-01-09 19:43:08 +0545

    list of program

--- blogs/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import *
-
-class BlogForms(forms.ModelForm):
-    class Meta:
-        model = Blog
-        fields = ['content']
-
--- portfolio/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from django.contrib import admin
-from portfolio.models import Contact, LikesModel, ViewsModel
-
-# Register your models here.
-admin.site.register(Contact)
-admin.site.register(ViewsModel)
-admin.site.register(LikesModel)
